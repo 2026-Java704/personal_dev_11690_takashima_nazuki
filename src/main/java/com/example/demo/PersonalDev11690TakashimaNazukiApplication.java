d80d3dd63f726b1e8dde359423b05e8b5df85270
student <student@FJ-1025>
2026-05-19 10:30:29 +0900

    新規登録

---
+++ src/main/java/com/example/demo/PersonalDev11690TakashimaNazukiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev11690TakashimaNazukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev11690TakashimaNazukiApplication.class, args);
+	}
+
+}

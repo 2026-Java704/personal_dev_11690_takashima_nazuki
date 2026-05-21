0ef76bd8db7a8de081d6fd35ae6238fe253e7257
student <student@FJ-1025>
2026-05-21 11:04:07 +0900

    食事登録処理追加

--- src/main/java/com/example/demo/controller/DishController.java
+++ src/main/java/com/example/demo/controller/DishController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,6 +28,7 @@ public class DishController {
 		this.resultRepository = resultRepository;
 	}
 
+	//登録内容一覧表示
 	@GetMapping("/dishes/result")
 	public String index(Model model) {
 		List<Result> resultList = resultRepository.findAll();
@@ -35,18 +37,21 @@ public class DishController {
 		return "dishesresult";
 	}
 
+	//登録内容一覧処理
 	@PostMapping("dishes/result")
 	public String result() {
 
 		return "dishesresult";
 	}
 
+	//新規食事登録画面表示
 	@GetMapping("/dishes/add")
-	public String add() {
+	public String create() {
 		return "dishesadd";
 
 	}
 
+	//新規食事登録処理	
 	@PostMapping("/dishes/add")
 	public String bbb(
 			@RequestParam(defaultValue = "") LocalDate recordDate,
@@ -60,11 +65,67 @@ public class DishController {
 
 		Integer userId = (Integer) session.getAttribute("userId");
 
+		Result result = new Result();
+		result.setUserId(userId);
+		result.setRecordDate(recordDate.now());
+		result.setStapleFood(stapleFood);
+		result.setSideDish(sideDish);
+		result.setMainDish(mainDish);
+		result.setMilkDish(milkDish);
+		result.setFruitCount(fruitCount);
+		result.setDetailMemo(detailMemo);
+		int achievement = sumAchievement(
+				stapleFood,
+				sideDish,
+				mainDish,
+				milkDish,
+				fruitCount);
+		result.setAchievement(achievement);
+		resultRepository.save(result);
+
 		return "dishesadd";
 	}
 
+	//更新画面表示
 	@GetMapping("dishes/{id}/edit")
-	public String ccc() {
+	public String edit(
+			@PathVariable Integer id,
+			Model model) {
+
+		Result result = resultRepository.findById(id).get();
+		model.addAttribute("result", result);
+
 		return "dishesedit";
+	}
+
+	//更新処理
+	@PostMapping("/dishes/{id}/edit")
+	public String update(
+			@PathVariable Integer id,
+			@RequestParam(defaultValue = "") LocalDate recordDate,
+			@RequestParam(defaultValue = "") Integer stapleFood,
+			@RequestParam(defaultValue = "") Integer sideDish,
+			@RequestParam(defaultValue = "") Integer mainDish,
+			@RequestParam(defaultValue = "") Integer milkDish,
+			@RequestParam(defaultValue = "") Integer fruitCount,
+			@RequestParam(defaultValue = "") String detailMemo,
+			Model model) {
+		Result result = resultRepository.findById(id).get();
+		result.setRecordDate(recordDate.now());
+		result.setStapleFood(stapleFood);
+		result.setSideDish(sideDish);
+		result.setMainDish(mainDish);
+		result.setMilkDish(milkDish);
+		result.setFruitCount(fruitCount);
+		result.setDetailMemo(detailMemo);
+		resultRepository.save(result);
+
+		return "dishesedit";
+	}
+
+	private int sumAchievement(Integer stapleFood, Integer sideDish, Integer mainDish, Integer milkDish,
+			Integer fruitCount) {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
 	}
 }
